686db8507215228618883e04ca5953c9a3055f2c
Per Hansson Adrian <per.hansson80@gmail.com>
2017-08-31 15:32:08 -0700

    clean files from root dir

--- svt-daq/root/cosmic_utils.hh
+++
@@ -1,9 +0,0 @@
-#ifndef cosmic_utils_hh
-
-namespace cosmic_utils {
-
-  double getMaxSampleOfThree(double* samples);
-  int getNrOfConsecutiveSamplesAboveThresh(double* samples,double thresh);
-}
-
-#endif
